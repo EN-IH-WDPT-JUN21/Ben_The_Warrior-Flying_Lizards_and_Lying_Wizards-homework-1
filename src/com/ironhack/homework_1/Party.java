01e5e5bf246c30d66d3231524879128d3450fe0a
João Afonso Silva <joaoafonsodss@gmail.com>
2021-06-25 21:11:23 +0100

    added more user interface and methods feedback

--- src/com/ironhack/homework_1/Party.java
+++ src/com/ironhack/homework_1/Party.java
@@ -6,12 +6,18 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Party {
-    private final int MAX_STARTING_SIZE = 12;
+    private final int MAX_STARTING_SIZE = 12; // Only used to define the maximum party size for the manual creation
     private List<Character> partyCharacters;
-//    private String partyName;     // Not sure if you want to give the party a name
+    private String partyName;
 
-    public Party(List<Character> partyCharacters) {
+
+    public Party(List<Character> partyCharacters, String partyName) {
         this.partyCharacters = partyCharacters;
+        this.partyName = partyName;
+    }
+
+    public Party(String partyName) {
+        this.partyName = partyName;
     }
 
     public Party() {
@@ -36,7 +42,7 @@ public class Party {
     }
 
     // ========== Party Creation ==========
-    // Method to select the initial size of the party
+    // Method to select the initial size of the party  (USER INPUT METHOD)
     public int inputPartySize() {
         System.out.println("Input the size of the party:    ( maximum " + MAX_STARTING_SIZE + " Characters )");
         while (true) {
@@ -68,7 +74,7 @@ public class Party {
             partyCharacters.add(Character.createCustom());
 
             //optional, used to stop creation method
-            int nextChoice = inputContinueCreating();
+            int nextChoice = inputExit(true);
             if (nextChoice == -1) return;       // exits
             if (nextChoice == 1) {              // create remaining party size as random. and exits
                 generateRandomParty(partySize - i - 1);
@@ -77,10 +83,12 @@ public class Party {
         }
     }
 
-    // Checks if the user wants to continue custom creation
-    // (doesn't need to be included. just in case you regret making 20 characters by hand)
-    private static int inputContinueCreating() {
-        System.out.println("Continue custom creation?   (y)Yes   (r)Randomise Remaining   (n)Exit");
+    // Checks if the user wants to continue custom creation  (USER INPUT METHOD)
+    // (doesn't need to be included. just for cases where you regret trying to create 30 characters by hand)
+    // (adapted so it could be used to exit other method)  (returns -1 to exit and 0 to continue)
+    private static int inputExit(boolean isCreating) {
+        System.out.println(isCreating ? "Continue custom creation?   (y)Yes   (r)Randomise Remaining   (n)Exit"
+                : "Continue operation?   (y)Yes   (n)Exit");
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
@@ -89,7 +97,9 @@ public class Party {
                     case "y":
                         return 0;
                     case "r":
-                        return 1;
+                        if (isCreating) return 1;
+                        System.out.println("Please choose a valid option!");
+                        break;
                     case "n":
                         return -1;
                     default:
@@ -105,23 +115,45 @@ public class Party {
     // Deletes all elements from party
     public void clearParty() {
         partyCharacters.clear();
+        System.out.println(partyName + " cleared!");
     }
-
 
     // ========== Characters in Party ==========
     //Adds character to party list
     public void addCharacterToParty(Character character) {
+        System.out.println("Added to " + partyName + ": " + character.printStats());
         partyCharacters.add(character);
     }
 
     //Removes character from party list (by character)
     public void removeCharacter(Character character) {
+        System.out.println("Removed from " + partyName + ": " + character.printStats());
         this.getPartyCharacters().remove(character);
     }
 
     //Removes character from party list (by index)
     public void removeCharacter(int i) {
+        System.out.println("Removed from " + partyName + ": " + this.getPartyCharacters().get(i).printStats());
         this.getPartyCharacters().remove(i);
+    }
+
+    // Selects character from the party  (USER INPUT METHOD)
+    public Character selectCharacter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a Character from your party:");
+        for (int i = 0; i < partyCharacters.size(); i++) {
+            System.out.println("(" + (i + 1) + ")" + " " + partyCharacters.get(i).printStats());
+        }
+        while (true) {
+            String input = scanner.nextLine();
+            try {
+                int choice = Integer.parseInt(input);
+                if (choice > 0 && choice <= partyCharacters.size()) return partyCharacters.get(choice - 1);
+                System.out.println("Please choose a valid option!");
+            } catch (NumberFormatException e) {
+                System.out.println("Please choose a valid option!");
+            }
+        }
     }
 
     // Returns a random character from the party
@@ -130,38 +162,19 @@ public class Party {
         return partyCharacters.get(rand.nextInt(partyCharacters.size()));
     }
 
-    // Selects character from the party
-    public Character selectCharacter() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose a Character from your party: ");
-        for (int i = 0; i < partyCharacters.size(); i++) {
-            System.out.println("(" + (i + 1) + ")" + "\t" + partyCharacters.get(i).printStats());
-        }
-        while (true) {
-            String input = scanner.nextLine();
-            try {
-                int choice = Integer.parseInt(input);
-                if (choice > 0 && choice <= partyCharacters.size()) return partyCharacters.get(choice - 1);
-            } catch (NumberFormatException e) {
-                System.out.println("Please choose a valid option!");
-            }
-        }
-    }
-
     // Returns the index of the character in the party list (can be useful for further methods) (not in use)
     public int getIdxInParty(Character character) {
-        return this.getPartyCharacters().indexOf(character);
+        return partyCharacters.indexOf(character);
     }
 
-
-    // ========== Party Log ==========
-    // TODO(JA) Needs to be tested - not sure if getMethods from Warrior and Wizard can be used on Character "character".
-    // Prints each character from the party with their stats.
-    // (Can be divided if we create a method to print a single character stats)
+    // ==================== Log ====================
+    // Prints each character from the party with their stats, using "Character.printStats()"
     public void printPartyStats() {
-//        System.out.println(partyName.toUpperCase());
+        System.out.println("Party - " + partyName);
         for (Character character : partyCharacters) {
             System.out.println(character.printStats());
         }
+        System.out.println("");
     }
+
 }
