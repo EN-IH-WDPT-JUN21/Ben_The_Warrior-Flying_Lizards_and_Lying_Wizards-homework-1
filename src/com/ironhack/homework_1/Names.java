09c97386beff57bac502dbafcd3d06e4720d8735
MigNeves <66562356+MigNeves@users.noreply.github.com>
2021-06-26 18:45:50 +0100

    Revert "Dev mig neves"

--- src/com/ironhack/homework_1/Names.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.homework_1;
-
-public class Names {
-    private static String[] nameList = {"Ben"};
-
-    public static String randomName() {
-        return nameList[(int) Math.floor(Math.random() * nameList.length)];
-    }
-}
