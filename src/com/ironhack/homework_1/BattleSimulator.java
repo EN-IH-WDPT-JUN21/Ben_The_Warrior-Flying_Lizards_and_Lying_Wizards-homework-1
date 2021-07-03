d2262826aa81e972d26a144bcef6fad60f60d81b
João Afonso Silva <joaoafonsodss@gmail.com>
2021-07-03 01:37:28 +0100

    Solve and clean conflicts

--- src/com/ironhack/homework_1/BattleSimulator.java
+++ src/com/ironhack/homework_1/BattleSimulator.java
@@ -52,10 +52,10 @@ public class BattleSimulator {
 
         while(c1.isAlive() && c2.isAlive()){
             round += 1;
-            if(!Menu.getSmallLog()) {
-                if (round == 1) {
+            if (!Menu.getSmallLog()) {
+                if (round == 1){
                     System.out.println("+=============================================================================================================================+");
-                } else {
+                }else{
                     System.out.println("+-----------------------------------------------------------------------------------------------------------------------------+");
                 }
                 StringBuilder roundString = new StringBuilder("|");
@@ -64,9 +64,9 @@ public class BattleSimulator {
                 boolean even = spaces % 2 == 0;
                 roundString.append(String.join("", Collections.nCopies(spaces / 2, " ")));
                 roundString.append("Round Nº ").append(round);
-                if (even) {
+                if(even){
                     roundString.append(String.join("", Collections.nCopies(spaces / 2, " ")));
-                } else {
+                }else{
                     roundString.append(String.join("", Collections.nCopies((spaces / 2) + 1, " ")));
                 }
                 roundString.append("|");
@@ -78,12 +78,11 @@ public class BattleSimulator {
             String c2attack = c2.attack(c1);
             String[] c2AttackDetails = {c2attack.substring(0, c2attack.indexOf('|')), c2attack.substring(c2attack.indexOf('|') + 1, c2attack.length())};
 
-            if(!Menu.getSmallLog()) {
+            if (!Menu.getSmallLog()) {
                 System.out.println("|                                                                                                                             |");
             }
 
             if(!c1.isAlive() && !c2.isAlive()){
-
                 this.graveyard.add(c1);
                 this.graveyard.add(c2);
                 this.party1.removeCharacter(c1);
@@ -129,33 +128,34 @@ public class BattleSimulator {
                 System.out.println(print2.toString());
             }
             else{
-              if(!Menu.getSmallLog()) {
-                StringBuilder print1 = new StringBuilder("| " + Menu.getParty1().getPartyName() + ": " + c1.getName() + " the " +
-                        c1.getClass().getSimpleName() + " attacks " +c2.getName() + " with " + c1AttackDetails[0] + " dealing " + c1AttackDetails[1] + " damage!");
-                StringBuilder print2 = new StringBuilder("| " + Menu.getParty2().getPartyName() + ": " + c2.getName() + " the " +
-                        c2.getClass().getSimpleName() + " attacks " +c1.getName() + " with " + c2AttackDetails[0] + " dealing " + c2AttackDetails[1] + " damage!");
-                StringBuilder print3 = new StringBuilder("| " + c1.getName() + " has " + c1.getHp() + " HP");
-                StringBuilder print4 = new StringBuilder("| " + c2.getName() + " has " + c2.getHp() + " HP");
+                if (!Menu.getSmallLog()) {
+                    StringBuilder print1 = new StringBuilder("| " + Menu.getParty1().getPartyName() + ": " + c1.getName() + " the " +
+                            c1.getClass().getSimpleName() + " attacks " + c2.getName() + " with " + c1AttackDetails[0] + " dealing " + c1AttackDetails[1] + " damage!");
+                    StringBuilder print2 = new StringBuilder("| " + Menu.getParty2().getPartyName() + ": " + c2.getName() + " the " +
+                            c2.getClass().getSimpleName() + " attacks " + c1.getName() + " with " + c2AttackDetails[0] + " dealing " + c2AttackDetails[1] + " damage!");
+                    StringBuilder print3 = new StringBuilder("| " + c1.getName() + " has " + c1.getHp() + " HP");
+                    StringBuilder print4 = new StringBuilder("| " + c2.getName() + " has " + c2.getHp() + " HP");
 
-                print1.append(String.join("", Collections.nCopies(126 - print1.toString().length(), " ")));
-                print1.append("|");
-                print2.append(String.join("", Collections.nCopies(126 - print2.toString().length(), " ")));
-                print2.append("|");
-                print3.append(String.join("", Collections.nCopies(126 - print3.toString().length(), " ")));
-                print3.append("|");
-                print4.append(String.join("", Collections.nCopies(126 - print4.toString().length(), " ")));
-                print4.append("|");
-                Menu.battleSpeedPause(500, 150);
-                System.out.println(print1);
-                Menu.battleSpeedPause(500, 150);
-                System.out.println(print2);
-                Menu.battleSpeedPause(500, 150);
-                System.out.println("|                                                                                                                             |");
-                System.out.println(print3);
-                Menu.battleSpeedPause(500, 150);
-                System.out.println(print4);
-                Menu.battleSpeedPause(500, 150);
-              }
+                    print1.append(String.join("", Collections.nCopies(126 - print1.toString().length(), " ")));
+                    print1.append("|");
+                    print2.append(String.join("", Collections.nCopies(126 - print2.toString().length(), " ")));
+                    print2.append("|");
+                    print3.append(String.join("", Collections.nCopies(126 - print3.toString().length(), " ")));
+                    print3.append("|");
+                    print4.append(String.join("", Collections.nCopies(126 - print4.toString().length(), " ")));
+                    print4.append("|");
+
+                    Menu.battleSpeedPause(500, 150);
+                    System.out.println(print1);
+                    Menu.battleSpeedPause(500, 150);
+                    System.out.println(print2);
+                    Menu.battleSpeedPause(500, 150);
+                    System.out.println("|                                                                                                                             |");
+                    System.out.println(print3);
+                    Menu.battleSpeedPause(500, 150);
+                    System.out.println(print4);
+                    Menu.battleSpeedPause(500, 150);
+                }
             }
         }
 
@@ -185,8 +185,8 @@ public class BattleSimulator {
         // battle is going to happen meanwhile party1 and party2 has at least one element
         while(!isPartyEmpty(this.party1) && !isPartyEmpty(this.party2)){
             fight++;
-            Character f1 =getRandomChar(party1);
-            Character f2 =getRandomChar(party2);
+            Character f1 = getRandomChar(party1);
+            Character f2 = getRandomChar(party2);
             String f1Presentation = f1.getName() + " the " + f1.getClass().getSimpleName();
             String f2Presentation = f2.getName() + " the " + f2.getClass().getSimpleName();
             StringBuilder fightPrint = new StringBuilder("|");
@@ -204,13 +204,13 @@ public class BattleSimulator {
 
             System.out.println("+=============================================================================================================================+");
             System.out.println(fightPrint);
-            if (Menu.getBattleSpeed()!=0) {
+            if (Menu.getBattleSpeed() != 0) {
                 System.out.print("|                                                    PRESS ENTER TO START                                                     |");
                 new Scanner(System.in).nextLine();
             }
 
         //choose random characters for Duel
-            duel(f1,f2);
+            duel(f1, f2);
         }
 
 
@@ -263,7 +263,7 @@ public class BattleSimulator {
         }
         else {
             System.out.println("|                                                        Player 2 WINS                                                        |");
-       }
+        }
         System.out.println("+=============================================================================================================================+");
     }
 
